c2dc183d8285fcce9049ddfc7713aa27923ce0d6
SuperAxelPaxel <axel.janhagen@gmail.com>
2023-01-05 22:24:07 +0100

    LaserBem

---
+++ ProjectThatReallyDoesNotWantToBeMade (3)/Assets/Script/EnemyScript/LaserBeamAttack.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeamAttack : MonoBehaviour
+{
+    [SerializeField] Transform Box;
+    [SerializeField] float DegreesPerSecond;
+    [SerializeField] Transform Effect;
+    [SerializeField] LayerMask Mask;
+    float invis;
+
+    private void Awake()
+    {
+        invis = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, -DegreesPerSecond * Time.deltaTime);
+        invis++;
+        RaycastHit2D hit = Physics2D.Raycast(Box.position, Box.forward);
+
+        print(hit.point);
+
+        Effect.transform.position = hit.point;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(collision.gameObject.name);
+        if (collision.gameObject.name == "Player" && invis > 150)
+        {
+            collision.gameObject.GetComponent<Health>().health--;
+            invis = 0;
+        }
+    }
+}
