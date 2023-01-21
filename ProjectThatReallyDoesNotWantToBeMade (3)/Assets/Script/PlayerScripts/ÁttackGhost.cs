50b0ffcb74349dc7e5fbb8874e6ab9742270e8e5
SuperAxelPaxel <axel.janhagen@gmail.com>
2023-01-21 10:25:20 +0100

    AttackeDaGhosties

--- ProjectThatReallyDoesNotWantToBeMade (3)/Assets/Script/PlayerScripts/ÁttackGhost.cs
+++ ProjectThatReallyDoesNotWantToBeMade (3)/Assets/Script/PlayerScripts/ÁttackGhost.cs
@@ -6,6 +6,9 @@ public class ÁttackGhost : MonoBehaviour
 {
     [SerializeField] Vector4 HurtColor;
     [SerializeField] Movement movScript;
+    bool KnockBacking;
+    GameObject enemy;
+    Vector3 dire;
 
     IEnumerator Died(GameObject col)
     {
@@ -15,17 +18,34 @@ public class ÁttackGhost : MonoBehaviour
         Destroy(col);
     }
 
+    private void Update()
+    {
+        if (KnockBacking)
+        {
+            MoveKnockBack(dire, enemy);
+        }
+    }
+
     IEnumerator Knockback(Vector3 dir, GameObject Ghost)
     {
         Ghost.GetComponent<FollowPlayer>().enabled = false;
-        Ghost.transform.position = Vector3.MoveTowards(Ghost.transform.position, dir * 2, 1f);
+        KnockBacking = true;
+        enemy = Ghost.gameObject;
+        dire = dir;
         yield return new WaitForSeconds(1);
-
+        KnockBacking = false;
         Ghost.GetComponent<FollowPlayer>().enabled = true;
+    }
+    void MoveKnockBack(Vector3 direction, GameObject Ghost)
+    {
+        direction.Normalize();
+        Debug.DrawRay(Vector3.zero, direction);
+        Ghost.transform.position = Vector3.Lerp(enemy.transform.position, direction * 0.75f, 2);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        enemy = collision.gameObject; 
         if (collision.gameObject.GetComponent<FollowPlayer>() != null && movScript.attacking)
         {
             if (collision.gameObject.GetComponent<FollowPlayer>().GhostHealth <= 0)
